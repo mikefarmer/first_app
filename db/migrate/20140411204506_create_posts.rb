aae5253dd2b9acf8baa00d0ee10a205f3e18dd7b
Prelang Builder <builder@prelang.com>
2014-04-11 20:45:06 +0000

    Generated model Post with migration

---
+++ db/migrate/20140411204506_create_posts.rb
@@ -0,0 +1,12 @@
+class CreatePosts < ActiveRecord::Migration
+  def change
+    create_table :posts do |t|
+      t.string :title
+      t.text :body
+      t.references :user, index: true
+      t.string :permalink
+
+      t.timestamps
+    end
+  end
+end
